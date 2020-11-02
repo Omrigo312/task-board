eaa1605c99b89d6c70f723fcd0f7cb4df3718c90
Omrigo312 <omrigo312@gmail.com>
2020-11-03 00:00:26 +0200

    task transition

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const taskTextElement = document.getElementById('taskText');
+const deadlineDateElement = document.getElementById('deadlineDate');
+const  deadlineTimeElement = document.getElementById('deadlineTime');
 function save() {
-  console.log("fd")
+  const taskText = taskTextElement.value;
+  const deadlineDate = deadlineDateElement.value;
+  const deadlineTime = deadlineTimeElement.value;
+  const deadline = deadlineTime
+    ? new Date(`${deadlineDate} ${deadlineTime}`)
+    : new Date(`${deadlineDate} 23:59:59`);
+  taskTextElement.classList.add('is-invalid');
+}
+
+function validateInput() {
+  
 }
